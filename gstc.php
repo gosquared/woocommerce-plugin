a81e9751a77c5dd1e688509acbe80d33f1a4ca55
Jack Kingston <jack@gosquared.com>
2012-01-06 16:30:14 +0000

    Added event tracking for add to cart, order, login and logout

--- gstc.php
+++ gstc.php
@@ -28,21 +28,25 @@ Contributions by: Jack Kingston, Aaran Parker
     along with GoSquared WooCommerce Plugin.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-//ini_set('display_errors', "on");
+ini_set('display_errors', "on");
 add_option('gstc_acct');
 add_option('gstc_trackAdmin');
 add_option('gstc_trackPreview');
 add_option('gstc_trackUser');
 add_action('admin_init', 'gs_init');
 add_action('admin_menu', 'gs_options');
-add_action('wp', 'gs_print_gstc');
+//add_action('wp_footer', 'gs_print_gstc');
+add_action('woocommerce_thankyou', 'gs_order_action');
+add_action('wp_login', 'gs_login_action');
+add_action('wp_print_footer_scripts', 'gs_print_gstc');
+add_action('wp_print_footer_scripts', 'gs_append_and_clear_events_in_session');
+add_action('wp_logout', 'gs_logout_action');
 
 function gs_init() {
     $style_url = WP_PLUGIN_URL . '/gosquared-woocommerce/gs.css';
     $style_file = WP_PLUGIN_DIR . '/gosquared-livestats/gs.css';
     /* Register our stylesheet. */
     wp_register_style('gs_style', $style_url);
-
 }
 
 function gs_options() {
@@ -237,15 +241,162 @@ function gs_print_gstc() {
         if ($gstc_userDetail) {
             $params['VisitorName'] = $gstc_userDetail;
         }
-        if (woocommerce_installed()) {
+        if (woocommerce_installed() && !is_admin()) {
             global $woocommerce;
             $params['Visitor'] = array();
+//            $params['Visitor']['page_type'] = getPageType();
             $params['Visitor']['cart_total'] = $woocommerce->cart->get_cart_total();
+//            $params['Visitor']['cart_items'] = array();
+            $cart_items = $woocommerce->cart->get_cart();
+            foreach ($cart_items as $item) {
+                $item_title = $item['data']->get_title();
+                $item_quantity = $item['quantity'];
+//                $item_price = $item['data']->get_price();
+//                $item_total_value = $item_price * $item_quantity;
+//                $item_fields = array("title" => $item_title,
+//                                     "quantity" => $item_quantity,
+//                                     "price" => $item_price,
+//                                     "total_value" => $item_total_value);
+//                array_push($params['Visitor']['cart_items'], $item_fields);
+                $params['Visitor'][$item_title] = "x$item_quantity";
+            }
+            $total_discount = $woocommerce->cart->get_total_discount();
+            if ($total_discount == false) $total_discount = "0";
+            $params['Visitor']['total_discount'] = $total_discount;
         }
-        wp_enqueue_script('gstc', WP_PLUGIN_URL . '/gosquared-woocommerce/tracker.js', '', false, true);
-        wp_localize_script('gstc', 'GoSquared', $params);
-        $meta = get_user_meta( get_current_user_id(), $input, true );
+        if (array_key_exists('add-to-cart', $_GET) && $woocommerce->verify_nonce('add_to_cart', '_GET')) {
+            wp_enqueue_script('gs_shop');
+            $item = array("id" => $_GET['add-to-cart']);
+            if (is_numeric($_GET['add-to-cart'])) {
+                $item[quantity] = (isset($_POST['quantity'])) ? (int) $_POST['quantity'] : 1;
+            } else if ($_GET['add-to-cart'] == 'group') {
+
+            }
+            $item_data = (array) apply_filters('woocommerce_add_cart_item_data', array(), $item['id']);
+        }
+        ?>
+        <script>
+            var GoSquared = <?php echo json_encode($params); ?>;
+            (function(w){
+                function gs(){
+                    w._gstc_lt=+(new Date); var d=document;
+                    var g = d.createElement("script"); g.type = "text/javascript"; g.async = true; g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+                    var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(g, s);
+                }
+                w.addEventListener?w.addEventListener("load",gs,false):w.attachEvent("onload",gs);
+
+                function appendEvent(cls,ev,f){
+                    var els = document.getElementsByClassName(cls);
+                    for (var e in els) {
+                        if (!isNaN(+e)) {
+                            var el = els[e];
+                            el[ev] = f;
+                        }
+                    }
+                }
+                function addToCard(e){
+                    var id = e.currentTarget.getAttribute("data-product_id");
+                    GoSquared.DefaultTracker.TrackEvent('Item added to cart', {'item_id': id});
+                }
+                var addToCartListeners = appendEvent('add_to_cart_button', 'onclick', addToCard);
+                w.addEventListener?w.addEventListener("load",addToCartListeners,false):w.attachEvent("onload",addToCartListeners);
+
+            })(window);
+        </script>
+        <?php
     }
 }
 
+function gs_order_action($order_id) {
+    $order = &new woocommerce_order($order_id);
+    $details = array();
+    $details['total'] = woocommerce_price($order->order_total);
+    $details['tax'] = woocommerce_price($order->order_tax);
+    $details['shipping'] = woocommerce_price($order->order_shipping);
+    foreach ($order->items as $item) {
+        $details[$item['name']] = $item['quantity']." @ ".woocommerce_price($item['cost']);
+    }
+    $details_json = json_encode($details);
+    appendEvents(array("Order Received" => $details_json));
+    add_action('wp_print_footer_scripts', 'events_footer');
+}
+
+function gs_login_action() {
+    gs_add_event_to_session("Logged in", "");
+}
+
+function gs_logout_action() {
+    gs_add_event_to_session("Logged out", "");
+}
+
+function gs_add_to_cart_action() {
+
+}
+
+function get_gsevents() {
+    $events = array();
+    if (isset($_SESSION['gsevents']) && is_array($_SESSION['gsevents'])) {
+        $events = $_SESSION['gsevents'];
+    }
+    return $events;
+}
+function gs_add_event_to_session($name, $val) {
+    $events = get_gsevents();
+    $events[$name] = $val;
+    $_SESSION['gsevents'] = $events;
+}
+
+function gs_append_and_clear_events_in_session() {
+    $events = get_gsevents();
+    appendEvents($events);
+    $_SESSION['gsevents'] = array();
+}
+
+function appendEvents($eventArray) {
+    ?>
+    <script>
+        window.gsevents = window.gsevents || [];
+<?php foreach ($eventArray as $name => $val) { ?>
+        window.gsevents.push(function () {
+            GoSquared.DefaultTracker.TrackEvent('<?php echo $name; ?>', '<?php echo $val; ?>');
+        });
+<?php } ?>
+    </script>
+    <?php
+    events_footer();
+}
+
+function events_footer() {
+    ?>
+    <script>
+        (function() {
+            function waitForTracker(f) {
+                if (!GoSquared.DefaultTracker) {
+                    setTimeout(function() {waitForTracker(f);},500);
+                } else {
+                    f();
+                }
+            }
+            waitForTracker(function() {
+                var gsevents = window.gsevents || [];
+                for (var ev in gsevents) {
+                    gsevents[ev]();
+                }
+            });
+        })();
+    </script>
+    <?php
+}
+
+function getPageType() {
+    if (woocommerce_installed()) {
+        if (is_shop()) return "C";
+        if (is_product_category()) return "C";
+        if (is_product_tag()) return "C";
+        if (is_product()) return "P";
+        if (is_cart()) return "S";
+        if (is_checkout()) return "S";
+    }
+    return "H";
+}
 ?>
